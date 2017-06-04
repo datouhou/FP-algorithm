e3a410602d28c254a133999fc328225c24294196
datouhou <datouhou@126.com>
2017-06-04 23:55:21 +0800

    first commit

---
+++ Apriori/Apriori.cpp
@@ -0,0 +1,328 @@
+ #include<iostream>
+#include<set>
+#include<map>
+#include<string>
+#include<vector>
+#include<fstream>
+#include<cstring>
+#include<cmath> 
+using namespace std;
+typedef map<set<string>,int> Map;
+
+Map Lk ;//k项集的频繁项集以及对应的数量  
+vector<set<string> > DataBase;//原始数据集 
+Map FP_set;//全体频繁项集合,用于剪枝时子集的查找 
+vector<string> item;//记录全体商品名 
+int minsupport = 50;//最小支持数 
+double min_be=0.05;//最小置信度 
+int flag;//辅助标记剪枝的判断 
+ofstream outfile;//用于输出结果到文本文档中 
+set<string> Data_Line;//辅助记录每一行的商品 
+
+
+void Input(){//输入函数，用于导入实验数据,将初始化的数据保存在DataBase中
+	ifstream f("./project4 - Groceries.csv");//打开文件输入流，导入数据文件 
+	
+	/*当数据文件打开失败的时候，直接打印错误信息，退出程序*/ 
+	if(f.fail()){
+		cout << "Connot open file"<<endl;
+		return ;
+	}
+    string line; 
+	string str="";//定义字符串，记录处理数据后，得到的商品名称 
+	std::getline(f,line);//读取第一行数据，将第一行的条目排除 
+	
+	/*每次读取一行数据，并进行处理，保存在DataBase中*/ 
+	while(std::getline(f,line)){
+		Data_Line.clear();
+		bool flag = true;//定义布尔型，用于判断是否读到'{','{'是商品名称的开始 
+		for(int l = 0; l < line.length();l++){
+			if(flag){//flag为true说明还未读到商品名称,为false说明读到商品名称，开始处理商品名 
+		       if(line[l]!='{')	continue;//没有读到'{'，保持flag状态，继续向下查找 
+   			   else {//读到'{'，改变flag状态 
+				  flag = false;
+				  continue;
+	  		    }
+			}
+			/*没有读到','或者'}'说明当前商品名还未结束，继续s的延长*/ 
+			if(line[l]!=','&& line[l]!='}'){
+				str+=line[l];
+			}
+			
+			/*否则，当前商品名结束，开始保存到DataBase中*/ 
+			else{
+				Data_Line.insert(str);
+				item.push_back(str);
+				str="";
+			}
+			/*如果读取到'}'，当前交易记录读取结束，离开循环，进行下一条的读取*/ 
+			if(line[l]=='}'){
+				DataBase.push_back(Data_Line);
+				break;
+			}
+		}
+	}
+}
+int judge(set<string> x,set<string> y){//判断函数，用于判断两个项之间的包含关系 
+  int flag=0;
+  
+  /*循环遍历查找，x中是否有元素不在y中*/ 
+  for(set<string>::iterator it=x.begin(); it!=x.end();it++){
+     if(y.find(*it)==y.end()){//x中存在元素未出现在y中 
+         flag=2;//将flag从0变为2，说明至少x不是y的子集 
+         break;
+     }
+  }
+  
+  /*循环遍历查找，y中是否有元素不在x中*/ 
+  for( set<string>::iterator it=y.begin(); it!=y.end();it++ ){
+      if( x.find(*it)==x.end()){//y中存在元素未出现在x中 
+         flag+=1;//将flag加1，说明y至少不是x的子集 
+		 /*如果原本flag为0,那么此时最终返回结果为1,说明x是y的真子集
+		   如果原本flag为2，那么此时最终返回结果为3，说明x和y彼此没有包含关系
+		   */              
+         break;
+      }
+  }
+  /*如果最终flag为0，说明x和y相等
+  	如果最终flag为2，说明y是x的真子集 
+    */
+  return flag;
+}
+void L1_gen(){//生成函数，用于生成1项的频繁项 
+     for(int j=0; j<item.size();j++){//扫描数据库中所有的商品 
+      int flag=1;//辅助变量，用于判断商品是否存在 
+      for(Map::iterator l_it=Lk.begin();l_it!=Lk.end();l_it++){//遍历所有当前的频繁项 
+        if((l_it->first).find(item[j]) != (l_it->first).end()){//在频繁项中发现了当前项 
+           Lk[l_it->first]++;//将对应的商品数量加1 
+           flag=0;//标记为已发现 
+           break;
+        }
+      }
+     if(flag){//当前频繁项集中并不存在当前商品 
+          Data_Line.clear();
+          Data_Line.insert(item[j]);
+          Lk.insert(pair<set<string>,int>(Data_Line,1));//在频繁项集中添加，数量设置为1 
+     }
+    }
+    
+    /*将当前频繁项集中数量小于最小支持度的从频繁项集中删去*/ 
+    Map::iterator m_it=Lk.begin(); 
+    while( m_it!=Lk.end()){//遍历频繁项集 
+      if( m_it->second< minsupport){//不满足最小支持数的，删去 
+        Lk.erase(m_it++);
+       } 
+      else m_it++;
+    }
+
+  /*将当前得到的一阶频繁项集输出到文本文件中去*/ 
+  for(Map::iterator l_it=Lk.begin();l_it!=Lk.end();l_it++){//遍历频繁项集 
+      for(set<string>::iterator s_it=(l_it->first).begin(); s_it!=(l_it->first).end(); s_it++){//遍历项集中每一件商品 
+         //outfile<<*s_it<<" "<<l_it->second<<endl;//属于商品名以及数量 
+      } 
+  }
+}
+ 
+Map apriori_gen(Map &Lk,int k){//用于生成1阶以上的频繁项集 
+   Map Lk_temp;//新生成的k阶频繁项集 
+   set<string> s_temp;//保存生成的频繁项 
+   if(k>2){//阶数大于2的情况 
+   	   /*利用k-1阶频繁项构造k阶候选项*/   
+	   for(Map::iterator l_it1=Lk.begin();l_it1!=Lk.end();l_it1++){//遍历k-1阶的频繁项集 
+	      for(Map::iterator l_it2=Lk.begin();l_it2!=Lk.end();l_it2++){//遍历k-1阶的频繁项集
+	         s_temp=l_it1->first;//首先将当前的前缀保存 
+	         
+	         /*查找当前k-1阶频繁项集中是否存在与当前s_temp具有相同前缀的频繁项*/ 
+	         for(set<string>::iterator s_it2=l_it2->first.begin(),s_it1=l_it1->first.begin();s_it2!=l_it2->first.end();s_it2++,s_it1++ ){
+	               if(*s_it1==*s_it2)	continue;
+	               if(*s_it1!=*s_it2){//出现匹配不同的情况 
+	               	  set<string>::iterator s_it3=s_it2;
+	               	  s_it3++;
+	     		      if(s_it3==l_it2->first.end()){//检查不同的位是否是最后一位，如果是，则生成候选项 
+	     		      	s_temp.insert(*s_it2);
+	     		      	
+	     		      	/*检查该候选项是否已经生成过*/ 
+	               		if(Lk_temp.find(s_temp)!=Lk_temp.end())   break;//已经出现过，直接跳过 
+					    else{//未出现过，则先添加到频繁项集中去 
+	                  	 Lk_temp.insert(pair<set<string>,int>(s_temp,0));
+	               		}
+	  			      }
+					   else break;//如果不匹配的不是最后一位，则直接跳过 
+				   }
+	         }
+	      }
+	   }
+	   
+	   /*剪枝操作*/
+ 	   int binary[200];
+	   int t; 
+	   for(Map::iterator l_it=Lk_temp.begin();l_it!=Lk_temp.end();l_it++){//遍历当前的频繁项集 
+		  memset(binary,0,sizeof binary);
+		  t=0;
+		  binary[0]=1;
+		  while(t<(pow(2,k)-2)){
+			  int bit = 0;
+			  s_temp=l_it->first;//拷贝当前候选项 
+	     	  for(set<string>::iterator s_it=l_it->first.begin();s_it!=l_it->first.end();s_it++ ){//模拟二进制遍历生成子集，判断是否为频繁项
+					if(binary[bit]==1){//对应二进制位为1 
+				        s_temp.erase(*s_it);//轮流去掉项中的商品，生成子集
+         			}
+			        bit++;//检查二进制下一位
+			  } 
+			  flag=1;//标记子集是否出现在频繁项集中 
+     		  for(Map::iterator FP_it=FP_set.begin();FP_it!=FP_set.end();FP_it++){//遍历在频繁项集中查找当前子集 
+	   		     if(0 == judge(s_temp,FP_it->first)){//在频繁项集中发现子集，将标记置0
+     	   		     flag=0;
+	                 break;
+	             }
+	          }
+	          if(flag){
+	          	/*for(set<string>::iterator s_it=l_it->first.begin();s_it!=l_it->first.end();s_it++ ){
+	          		cout << *s_it << "#";
+				  }
+				  cout << endl;
+	          	for(set<string>::iterator s_it2=s_temp.begin();s_it2!=s_temp.end();s_it2++){
+				 		cout << *s_it2<<","; 
+				 } 
+					 cout<< endl;*/
+			    break;//未发现，跳出循环
+			}
+	          /*模拟二进制进位*/ 
+	          binary[0]++;//末位加1 
+			  int b=0;
+	 	      while(binary[b]>1){//出现大于1，执行进位 
+				 binary[b]=0;
+	             b++;
+	             binary[b]++;
+			   }
+			   t++;
+         }
+	     if(flag) Lk_temp.erase(l_it);//存在子集不是频繁项集，直接剪枝 
+	    } 
+	    //if(Lk_temp.empty())	cout << "3333"<<endl;
+	   
+   } 
+   else{//2阶频繁集的生成 
+   		for( Map::iterator l_it1=Lk.begin();l_it1!=Lk.end();l_it1++ ){//遍历1阶频繁项集 
+      		 for( Map::iterator l_it2=Lk.begin();l_it2!=Lk.end();l_it2++ ){//遍历1阶频繁项集  
+		         if(!((l_it1->first > l_it2->first)||(l_it1->first < l_it2->first)))  continue;//如果两项相同则跳过，否则可直接组装 
+				 string temp=*(l_it2->first.begin());//取出商品名 
+				 s_temp=l_it1->first; 
+				 s_temp.insert(temp);//组装为2阶候选项 
+				 if(Lk_temp.find(s_temp)!=Lk_temp.end())  continue;//判断当前2阶项是否已经生成过了  
+     			 else{//没有生成过，先放入频繁项集中 
+         		 		Lk_temp.insert(pair<set<string>,int >(s_temp,0));  
+   				  }  
+		      }  
+	    }  
+   }
+   /*开始进行当前候选集的计数*/ 
+      for(Map::iterator l_it=Lk_temp.begin();l_it!=Lk_temp.end();l_it++){//遍历候选集 
+           for(int i=0;i<DataBase.size();i++){//遍历所有的原始数据项 
+          		 if(judge(DataBase[i],l_it->first)%2==0){//在原始数据项中发现当前候选项，则将对应频数加1 
+                  	 Lk_temp[l_it->first]++;
+    		     }
+		    }
+		}
+		
+		/*遍历，将候选集中数量小于最小支持数的项删去*/ 
+ 	   for( Map::iterator l_it=Lk_temp.begin();l_it!=Lk_temp.end();){//遍历候选集 
+		//cout<<num<<endl;
+         if(Lk_temp[l_it->first] < minsupport){//小于最小支持数，删去 
+             Lk_temp.erase(l_it++);
+             if(Lk_temp.empty())	break;//当前候选集为空，退出 
+         }
+         else l_it++;
+     } 
+     /*遍历频繁项集，输出结果*/ 
+	   for( Map::iterator l_it=Lk_temp.begin();l_it!=Lk_temp.end();l_it++ ){//遍历频繁项集 
+	        for( set<string>::iterator s_it=l_it->first.begin();s_it!=l_it->first.end();s_it++ ){//遍历商品名，输出 
+	             //outfile<<*s_it<<"  ";
+	             //cout << *s_it<<" ";
+	         }
+	        //outfile<<l_it->second<<endl;//输出数量 
+	        //cout<<l_it->second<<endl;//输出数量 
+	   
+	   }
+   return Lk_temp;
+}
+void result(int k){//根据频繁项集搜索出满足最小置信度的关联关系 
+	 int binary[200];//保存模拟的二进制数 
+     int t; 
+     int totalnum;//定义整型，用于保存频繁项集的总数量 
+	 for(Map::iterator l_it=Lk.begin();l_it!=Lk.end();l_it++){//遍历当前的频繁项集 
+		  totalnum=l_it->second;//获取当前频繁项集的总数量 
+		  memset(binary,0,sizeof binary);
+		  t=0;
+		  binary[0]=1;
+		  
+		  while(t<(pow(2,k-1)-2)){
+		  	set<string> s_temp;
+		    set<string> s_temp2;
+			  int bit = 0;
+			  s_temp=l_it->first;//拷贝当前候选项 
+			  
+	     	  for(set<string>::iterator s_it=l_it->first.begin();s_it!=l_it->first.end();s_it++ ){//模拟二进制遍历生成子集，判断是否为频繁项
+					if(binary[bit]==1){//对应二进制位为1 
+				        s_temp.erase(*s_it);//轮流去掉项中的商品，生成子集
+				        s_temp2.insert(*s_it);//保存子集的补集 
+         			}
+			        bit++;//检查二进制下一位
+			  } 
+			  Map::iterator M_it;
+			  M_it=FP_set.find(s_temp);//在全体频繁项集中找到子集 
+			  int partnum=M_it->second;//获取子集的数量 
+			  double believe = 1.0*totalnum/partnum;//计算置信度 
+			  if(believe>min_be){//置信度满足条件，进行输出 
+			  	for(set<string>::iterator s_it=s_temp.begin();s_it!=s_temp.end();s_it++){//打印子集 
+				  //cout<<*s_it<<" ";
+				  outfile<<*s_it<<"  "; 
+	  	        }
+			  	outfile << " ====>";
+			  	for(set<string>::iterator s_it=s_temp2.begin();s_it!=s_temp2.end();s_it++){//打印补集 
+				  //cout<<*s_it<<"   ";
+			  	  outfile<<*s_it<<"  ";
+	  	        }
+	  	        //cout << believe << endl;//输出置信度 
+	  	        outfile<<believe<<endl;
+			  }
+	          /*模拟二进制进位*/ 
+	          binary[0]++;//末位加1 
+			  int b=0;
+	 	      while(binary[b]>1){//出现大于1，执行进位 
+				 binary[b]=0;
+	             b++;
+	             binary[b]++;
+			   }
+			   t++;
+            }
+	    }
+}
+int main()
+{ 
+    printf("请指定最小支持数（建议50左右）\n");
+	cin>>minsupport;
+	printf("请指定最小置信度（建议0.1左右）\n");
+	cin>>min_be; 
+    outfile.open("out.txt");//打开输出文件 
+	Input();//导入数据 
+	L1_gen();//生成1阶项集 
+  	int k=2;//当前阶数为2 
+  	Map new_Lk;
+  	while(true){//循环，生成k阶频繁项集 
+	     
+	 	 for(Map::iterator l_it=Lk.begin();l_it!=Lk.end();l_it++){
+			  FP_set.insert(*l_it);
+	 	 }
+   	        new_Lk=apriori_gen(Lk,k);
+  			if(new_Lk.empty())	break;
+  			else {
+  				k++;
+  				Lk=new_Lk;
+			  }
+	}
+	   
+    result(k);
+	return 0;
+}
+
---
+++ FP-growth/FP-growth.cpp
@@ -0,0 +1,340 @@
+ #include <cstdio>  
+#include<string>  
+#include <cstring>
+#include<map>  
+#include<vector>        
+#include<iostream>  
+#include <fstream>
+#include<list> 
+#include<cmath> 
+using namespace std;
+ 
+ 
+  
+  
+class DataNode{//定义数据节点，用于记录每件商品的名称和次数   
+   public:
+    int name;//用整型数据name映射商品名称的字符串 
+    int num; //用于记录商品的数量 
+    DataNode(int x,int y){//自定义数据节点的生成函数 
+        name=x;  
+        num=y;  
+    }  
+};  
+class TreeNode{//定义FP-tree的树节点类   
+   public:
+    int parent;//父节点的节点编号 
+    vector<int>child;//子节点的节点编号 
+    int name;//节点商品的名称编号 
+	int num;//节点商品的数量 
+}; 
+TreeNode Copy(TreeNode tmp){//定义树节点的拷贝函数，方便之后进行递归的状态保存和恢复   
+    TreeNode orginal;//原始节点  
+    orginal.parent=tmp.parent;//拷贝父节点 
+    orginal.name=tmp.name;//拷贝节点商品的名称编号  
+	orginal.num=tmp.num;//拷贝节点商品的数量  
+    return orginal;  
+} 
+typedef list<DataNode>  li_d;  
+int data_size=9835;//数据的行数 
+int minsupport=50;//最小支持数 
+int tree_sz;//FP树的节点数量  
+int total;//数据中商品的种类数 
+int count[10000];//每类商品的数量  
+int weight[10000];//商品的权重数组，用于建树之后对每项进行排序的依据
+int binary[200];
+int path_num; 
+int cond_node_num;
+ofstream outfile;
+int dictionary[100000][200];//将树结构字典化，用于记录每个树节点的子节点，减少遍历，空间换时间 
+map<string,int> index;//用于商品名称和商品编号的映射 
+li_d DataBase[10000];//数据库，用于记录每轮建树的数据 
+TreeNode *cond_tree=new TreeNode[50000];//记录FPtree结构 
+
+string getname(int item){//输入：商品编号，输出：商品名称的字符串  
+    string s;//定义字符串，保存商品名称
+	/*循环检索index，进行商品的编号的匹配，还原商品名*/  
+    for(map<string,int>::iterator it=index.begin();it!=index.end();it++)  
+    {  
+        if(it->second==item)//如果检索到了匹配的编号，就将对应的商品民字符串匹配输出 
+        {  
+            s=it->first;  
+            return s;  
+        }  
+    }
+    
+	s="This is something wrong with index,we can't find it.";//如果未找到，说明index出现错误，提交错误信息  
+    return s;  
+}  
+void Input(){//输入函数，用于导入实验数据,将初始化的数据保存在DataBase数组中  
+	ifstream f("./project4 - Groceries.csv");//打开文件输入流，导入数据文件 
+	
+	/*当数据文件打开失败的时候，直接打印错误信息，退出程序*/ 
+	if(f.fail()){
+		cout << "Connot open file"<<endl;
+		return;
+	}
+	
+	string line;//定义字符串，保存读入的原始数据 
+	int num=1;//定义编码整型，记录商品的编码 
+	string s="";//定义字符串，记录处理数据后，得到的商品名称 
+	std::getline(f,line);//读取第一行数据，将第一行的条目排除 
+	int i = 0;//记录当前读取数据的行数，方面DataBase进行索引
+	
+	/*每次读取一行数据，并进行处理，保存在DataBase中*/ 
+	while(std::getline(f,line)){
+		i++;//每次读取行数加一 
+		bool flag = true;//定义布尔型，用于判断是否读到'{','{'是商品名称的开始 
+		for(int l = 0; l < line.length();l++){
+			if(flag){//flag为true说明还未读到商品名称,为false说明读到商品名称，开始处理商品名 
+		       if(line[l]!='{')	continue;//没有读到'{'，保持flag状态，继续向下查找 
+   			   else {//读到'{'，改变flag状态 
+				  flag = false;
+				  continue;
+	  		    }
+			}
+			/*没有读到','或者'}'说明当前商品名还未结束，继续s的延长*/ 
+			if(line[l]!=','&& line[l]!='}'){
+				s+=line[l];
+			}
+			
+			/*否则，当前商品名结束，开始保存到DataBase中*/ 
+			else{
+				map<string,int>::iterator index_it;//定义迭代器，用于保存商品名称查找的位置  
+            	index_it=index.find(s);//在index中查找当前的商品名称
+				  
+            	if(index_it==index.end()){//迭代器出现在index结尾，说明未找到当前名称，需要新建映射 
+	                index[s]=num;//建立新的映射关系  
+	                num++;//商品编号加1  
+            	}  
+             	DataBase[i].push_back(DataNode(index[s],1));//将当前的编号和数量1保存在DataBase当前记录中    
+				s="";//清空字符串s，用于下一个商品名称的记录 
+			}
+			/*如果读取到'}'，当前交易记录读取结束，离开循环，进行下一条的读取*/ 
+			if(line[l]=='}'){
+				break;
+			}
+		}
+	}     
+	total = num;
+	//printf("%d\n",total);//打印商品种类的总数量，用于调试，防止数组溢出 
+}  
+
+
+bool cmp(DataNode a,DataNode b){//定义比较函数，比较数据节点的商品数量，进行headtable的排序
+    return a.num>=b.num;  
+}  
+
+ 
+li_d  CreateHeadTable(li_d *database,int datasize){//函数用于创建数据头表   
+    memset(count,0,sizeof(count));//清空计数数组，准备记录每类商品的数量  
+    li_d headtable;//创建数据节点列表，用于记录headtable
+	 
+	/*遍历每一条交易数据中的每一个商品，统计每类商品的总数量*/ 
+    for(int i=0;i<datasize;i++)
+    {  
+           
+         for(li_d ::iterator iter=database[i].begin();iter!=database[i].end();iter++)//遍历每一条交易记录的每个商品  
+         {    
+             count[iter->name]+=iter->num;//将当前商品的数目加1 
+         }  
+    } 
+	
+	/*遍历每一类商品，将其中满足最小支持数的商品筛选出来*/ 
+    for(int i=0;i<total;i++)  
+    {  
+        if(count[i]<minsupport) continue;  
+        headtable.push_back(DataNode(i,count[i]));//如果商品数大于最小支持树，将该数据加入到headtable中 
+    }  
+    headtable.sort(cmp);//按照商品数量对headtable中的每一项进行排序，从大到小 
+	return headtable;
+}
+
+
+bool cmp2(DataNode x,DataNode y){//定义比较函数，用于对每条交易记录的商品进行内部排序  
+    if(weight[x.name]<=weight[y.name])
+        return true;  
+    return false;  
+}  
+void sort_weight(li_d *database,li_d headtable,int datasize){//对weight数据进行构建，并利用该数组对每条交易记录的商品进行内部排序 
+	
+    memset(weight,0,sizeof(weight));//清空权重数组，用于内部排序  
+    int order=0;//初始化权重编号
+	  
+	/*遍历项头表，对权重数据进行构建*/  
+    for(li_d ::iterator iter=headtable.begin();iter!=headtable.end();iter++){    
+        weight[iter->name]=order;//将当前权重付给对应商品 
+        order++; //权重编号顺次加1 
+    } 
+	
+ 	/*遍历数据库，对数据库中的每一条交易记录进行检查，剔除小于最小支持数的，加快计算 */
+    for(int i=0;i<datasize;i++){//遍历数据库  
+         for(li_d ::iterator iter=database[i].begin();iter!=database[i].end();){//遍历每一条交易记录   
+             if(count[iter->name]<minsupport){//如果当前商品的数量小于最小支持度，就将其从交易记录中删除  
+                 database[i].erase(iter++);  
+             }  
+             else  
+            {  
+                iter++;  
+            }  
+         }  
+         database[i].sort(cmp2);//根据cmp2,对当前的交易记录进行排序 
+    }  
+}
+ 
+TreeNode* BuildTree(li_d a [],int num,TreeNode *FP_tree){//利用当前的数据列表进行FP树的构建  
+    //TreeNode *Val=new TreeNode[100];//用数组存储数节点，可以通过每个节点的parent和child还原整个树，Val[0]存根节点  
+    memset(dictionary,0,sizeof(dictionary));//清空dictionary数组，用于记录当前的节点父子关系  
+    int node_num=1;//记录当前FP树的节点数  
+    
+	/*遍历当前数据，构建树结构*/ 
+	for(int i=0;i<num;i++){//遍历当前数据的每一行  
+        int root=0;//每条记录的构建都以null节点开始 
+        for(li_d ::iterator it=a[i].begin();it!=a[i].end();it++){//遍历每一条交易记录  
+            DataNode t=*it;  
+            int s=t.name;
+			
+			/*如果遍历到的商品不是当前节点的子节点，新增子节点*/  
+            if(dictionary[root][s]==0){  
+                memset(dictionary[node_num],0,sizeof(dictionary[node_num]));//清空当前结点编码的dictionary列表，用于记录它的子节点  
+                TreeNode tmp=TreeNode();//新建节点  
+                tmp.num=t.num;//赋值操作  
+                tmp.name=s;  
+                tmp.parent=root;//父节点定义为当前节点  
+                FP_tree[node_num]=tmp;//新增节点赋值给当前节点编号的树节点  
+                FP_tree[root].child.push_back(node_num);//在当前的树节点的子节点列表中中添加新增节点 
+                dictionary[root][s]=node_num;//更新当前节点的dictionary数据，方便之后的查找操作 
+                node_num++;//节点数据加1 
+            }
+			
+			/*遍历到的商品是当前节点的子节点，则将FP树中对应节点的数量加1*/  
+            else{  
+                for(int k=0;k<FP_tree[root].child.size();k++){  
+                    if(FP_tree[FP_tree[root].child[k]].name==s){//查找到对应商品的子节点  
+                        FP_tree[FP_tree[root].child[k]].num+=t.num;//将商品数量增加 
+                         break;  
+                    }  
+                }  
+            }  
+            root=dictionary[root][s];//将当前节点沿路径向下移动 
+        }  
+    }  
+    tree_sz=node_num;//更新树的大小 
+    return FP_tree; 
+}  
+
+void one_path_out(li_d database[],int leaf_node,int cond_code_num,li_d  post_index){//用于当前树只剩下一条路径时，直接输出路径上的全部排列组合，减少递归 
+	//cout <<"cond_code_num"<<cond_code_num<<endl;
+	string str="";//定义字符串，用于记录当前的前缀 
+	if(!post_index.empty()){//前缀不为空    
+            for(li_d ::iterator it=post_index.begin();it!=post_index.end();it++){//循环得到前缀集    
+                str+=getname(it->name)+" "; 
+            }      
+    }  
+	memset(binary,0,sizeof(binary));//清空binary数组，用于模拟二进制进位 
+	int bit=0;//定义整型用于控制循环次数
+	
+	/*模拟二进制进位过程，进行路径节点组合的枚举*/ 
+	while(bit<pow(2,cond_code_num)){//循环次数应等于2的cond_code_num次方 
+		bit++;
+		binary[0]++;//二进制末位加1 
+		string s="";//定义字符串用于当前记录当前的频繁项 
+		int j = 0;//定义整型，用于索引binary数组 
+		int num = cond_tree[leaf_node].num;//获取当前频数 
+		for(li_d ::iterator iter=database[0].begin();iter!=database[0].end();iter++){
+			if(binary[j]==1)	s+=getname(iter->name)+" ";//字符串加上当前二进制位为1的对应的商品 
+			j++;//检查二进制下一位 
+		}
+		s+=str;//加上前缀集 
+	    outfile<<s<< num << endl;//输出 
+		int i = 0;//定义整型，用于进位移动 
+		while(binary[i]>1){//检查当前位是否等于2 
+			binary[i]=0;//等于2就将当前位变为0 
+			i++;//前进一位 
+			binary[i]+=1;//当前位加1 
+		}
+	}
+	
+}
+void FP_Growth(li_d database[],li_d post_index,int data_size){//通过递归，实现FP-growth算法，在FP树中寻找频繁项 
+    li_d head_table=CreateHeadTable(database,data_size);//利用函数CreateHeadTable，创建项表头 
+    sort_weight(database,head_table,data_size);//对每条交易记录内部进行排序 
+    BuildTree(database,data_size,cond_tree);//构建FP树 
+    TreeNode *tree_buffer=new TreeNode[50000];//创建FPtree的buffer数组，用于在递归中记录FP树并在递归结束是还原 
+	int tmpsz=tree_sz;//记录当前FP树的规模，用于FP树恢复时候循环的边界 
+    
+	/*拷贝FP树*/ 
+	for(int i=0;i<tree_sz;i++){  
+        tree_buffer[i]=Copy(cond_tree[i]);  
+    }
+	
+	/*如果当前构造出的树只有一个根节点，说明树结构为空，递归结束*/  
+    if(tree_sz==1){  
+        return;  
+    }
+	/*遍历head_table，对当前的频繁项集进行输出,因为查找关联关系，当前缀中没有元素的时候，频繁项不必输出*/ 
+    if(!post_index.empty()){  
+        for(li_d ::iterator iter=head_table.begin();iter!=head_table.end();iter++){//遍历head_table  
+            DataNode t=*iter;  
+            string str=getname(t.name);
+            int num=t.num;
+            for(li_d ::iterator it=post_index.begin();it!=post_index.end();it++){//循环输出前缀集    
+                str+=","+getname(it->name); 
+            }
+            outfile<<str<<" "<<num<<endl;  
+        }  
+    }  
+    
+    /*按照数量从小到大的顺寻，对head_table进行遍历，从树的叶节点开始，依次作为前缀，向上查找，构建条件FPtree*/ 
+    for(li_d ::reverse_iterator iter=head_table.rbegin();iter!=head_table.rend();iter++){  
+        DataNode t=*iter;  
+        li_d new_post_index;//定义新的前缀列表 
+        new_post_index.push_back(DataNode(t.name,t.num));//将当前遍历节点加入到前缀列表中 
+        if(!post_index.empty()){//当前前缀列表非空，要与新的列表进行合并操作  
+            li_d tmp=post_index;  
+            new_post_index.merge(post_index,cmp);  
+            post_index=tmp;  
+        }  
+        li_d newdatabase[10000];//定义数据节点列表，用于下一轮递归的数据保存 
+        int new_data_size=0;
+        path_num=0;
+        cond_node_num=0;
+        int leaf_node;
+		/*循环遍历当前的树节点，将其中的所有当前遍历商品找出，并向上查找所有关联路径，构建新的数据*/  
+        for(int i=0;i<tree_sz;i++){//遍历当前树节点  
+            if(cond_tree[i].name==t.name){//找到所有当前的商品的节点
+		        leaf_node=i;
+				path_num++;  
+                for(int pre=cond_tree[i].parent;pre!=0;pre=cond_tree[pre].parent){//向上查找路径 
+					cond_node_num++; 
+                    newdatabase[new_data_size].push_back(DataNode(cond_tree[pre].name,cond_tree[i].num));//将路径上的数据加入新的数据中 
+                }  
+                new_data_size++;//新数据规模加1  
+            } 
+        }  
+        
+        /*若当前树仅剩一条路径，可直接将路径上所有节点组合输出，无需递归*/ 
+        if(path_num==1){//仅剩一条路径 
+	        if(cond_tree[leaf_node].num<minsupport)	continue;//如果当前的节点支持度小于最小支持度，直接剪枝 
+	        if(cond_node_num>0)	one_path_out(newdatabase,leaf_node,cond_node_num,new_post_index);//当前节点存在父节点，之间进行全组合输出 
+		}
+		else	FP_Growth(newdatabase,new_post_index,new_data_size);//递归调用  
+        
+        /*恢复现场*/ 
+        tree_sz=tmpsz;//恢复树的规模  
+        for(int i=0;i<tree_sz;i++){//恢复树节点  
+            cond_tree[i]=Copy(tree_buffer[i]);  
+        }
+    }  
+
+}  
+int main(){    
+    outfile.open("out.txt");
+    printf("请指定最小支持数\n");
+    cin>>minsupport;
+    Input();//输入数据  
+    li_d post_index=li_d ();//初始化前缀列表为空 
+    FP_Growth(DataBase,post_index,data_size);//递归查找 
+    return 0;  
+}  
+
